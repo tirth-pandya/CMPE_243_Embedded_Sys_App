81fee5d12a0154cf4f2a44d6085fa34069b99185
Tirth_Pandya <tirth_pandya@yahoo.com>
2021-02-09 16:31:10 -0800

    Lab 01 : Initial Commit

--- projects/lpc40xx_freertos/l5_application/periodics/periodic_callbacks.c
+++ projects/lpc40xx_freertos/l5_application/periodics/periodic_callbacks.c
@@ -1,7 +1,13 @@
-#include "periodic_callbacks.h"
+#include "FreeRTOS.h"
+#include "task.h"
 
 #include "board_io.h"
 #include "gpio.h"
+#include "periodic_callbacks.h"
+#include "switch_led_logic.h"
+
+// Uncomment the following for Lab-Part3
+//#define LAB_01_P3
 
 /******************************************************************************
  * Your board will reset if the periodic function does not return within its deadline
@@ -10,11 +16,19 @@
  */
 void periodic_callbacks__initialize(void) {
   // This method is invoked once when the periodic tasks are created
+  switch_led_logic__initialize();
 }
 
 void periodic_callbacks__1Hz(uint32_t callback_count) {
   gpio__toggle(board_io__get_led0());
   // Add your code here
+  switch_led_logic__run_once();
+
+#ifdef LAB_01_P3
+  if (callback_count >= 5) {
+    vTaskDelay(1000);
+  }
+#endif
 }
 
 void periodic_callbacks__10Hz(uint32_t callback_count) {
